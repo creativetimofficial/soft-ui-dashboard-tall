6b8a32cc9fab2e4d1eb60922255eba81b9f939ce
corina <corina.coste@updivision.com>
2022-11-15 10:53:00 +0200

    reseeding with var from env added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,9 +15,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $min = config('app.scheduled');
         if (env('IS_DEMO')){
-            $schedule->command('migrate:fresh --seed')->everyFifteenMinutes();
+            $schedule->command('migrate:fresh --seed')->cron('*/'. $min .' * * * *');
         }
     }
 
